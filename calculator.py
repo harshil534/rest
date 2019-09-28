17d79ea485dc1c1be11cf1c32138a3bc0784f516
Jatin <jatinninawe02@gmail.com>
2019-09-28 11:41:18 +0530

    update

--- calculator.py
+++ calculator.py
@@ -3,10 +3,10 @@ from b import *
 from multiply import *
 from division import *
 def calculator():
-	a=int(input("enter a number"))
-	b=int(input("enter a number"))
+	a=23
+	b=12
 	print("enter 1 for addition,2 for subtraction,3 for multiplication and 4 for division")
-	c=int(input("enter your choice"))
+	c=2
 	if(c==1):
 		print(add(a,b))
 	elif(c==2):
